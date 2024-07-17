2372ced6a442073901dea027e1124a30c70e354e
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-07-17 10:24:12 -0500

    Changes at code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -32,6 +32,31 @@ public class Main {
         System.out.print("Digita tu edad: ");
         edadUsuario = input.nextInt();
 
+        System.out.println("Digita el año en que naciste");
+        int anioNacimientoUsuario = input.nextInt();
+
+        System.out.println("Digita el mes (1-12) en que naciste: ");
+        int mesNacimientoUsuario = input.nextInt();
+
+        System.out.println("Digita el día en que naciste: ");
+        int diaNacimientoUsuario = input.nextInt();
+
+        fechaNacimientoUsuario = LocalDate.of(anioNacimientoUsuario, mesNacimientoUsuario, diaNacimientoUsuario);
+
+
+        //Salidas del programa
+
+        System.out.println("\nApreciado " + nombreUsuario + ", bienvenido.");
+        System.out.println("Hemos enviado a tu email " + correoUsuario );
+        System.out.println("Tus datos de inicio de sesión.");
+        System.out.println("............................................");
+        System.out.println("\nA tu barrio " + barrioUsuario);
+        System.out.println("Llegara la tarjeta de credito de nuestro banco.");
+        System.out.println("............................................");
+
+        System.out.println("\nRecuerda que naciste en " + fechaNacimientoUsuario);
+
+
 
 
 
